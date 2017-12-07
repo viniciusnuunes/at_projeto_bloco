f8075f99df0f157efb8df0777388038b820ec815
Vinicius Nunes <viniiciuspereiira@hotmail.com>
2017-12-07 19:09:42 -0200

    Adicionado 5 questões. Background alterado. Ajustes no layout do quiz.

--- app/src/main/java/com/example/viniciusnunes/memequiz/QuizActivity.java
+++ app/src/main/java/com/example/viniciusnunes/memequiz/QuizActivity.java
@@ -24,16 +24,16 @@ public class QuizActivity extends AppCompatActivity {
     int pontos;
     List<Questao> questoes = new ArrayList<Questao>(){
         {
-            add(new Questao(R.drawable.morre, "O suspeito acima ficou chateado com as perguntas e falou para os reporteres", R.id.rbResposta1, "Morre, diabo!", "Quero que todos saiam daqui!", "Vão todos tomar no c*", "Vão se foderem"));
-            add(new Questao(R.drawable.choque, "Aqui por exemplo, temos...", R.id.rbResposta4, "Uiiii uii ui", "Ai caramba!", "Ai meu Deus!", "Aiii ai aiii"));
-            add(new Questao(R.drawable.gostozinho, "Segundo a pensadora TN Martins, o novinho está", R.id.rbResposta1, "Gostozinho no azeite", "Mec Nelson nos acessos", "Brotando a xota", "Aquelas coisas"));
-            add(new Questao(R.drawable.poha, "O comentarista informou aos telespectadores que o programa:", R.id.rbResposta3, "Ta uma merda", "Ta uma droga", "Ta uma poha", "Ta uma zorra"));
-            add(new Questao(R.drawable.burro, "Não cara...", R.id.rbResposta2, "Como você é ridículo!", "Como você é burro!", "Como você é idiota!", "Como você é pífio"));
-            add(new Questao(R.drawable.pao, "Mamãe, no céu tem pão?", R.id.rbResposta3, "E sobreviveu", "E viveu", "E morreu", "E faleceu"));
-            add(new Questao(R.drawable.mamilos, "Hoje eu vim falar de um assunto polêmico..", R.id.rbResposta4, "Nádegas", "Olhos", "Mãos", "Mamilos"));
-            add(new Questao(R.drawable.cremoso, "Complete a frase segundo Gilderlan. E depois.. tem:", R.id.rbResposta1, "Xenhenhenhenhem", "Nheco Nheco", "Nananananana", "Um furdunço"));
-            add(new Questao(R.drawable.lohany, "Qual é o nome artístico dessa maravilhosa criatura?", R.id.rbResposta2, "Lohane Vêkanandre Gisele Gavião da Portela de bala Halls", "Lohane Vêkanandre Sthephany Smith Bueno de HA HA HA de Raio Laser bala de Icekiss", "Lohane Vêkanandre Bueno de HA HA HA de Raio Laser de bala Mentos", "Lohane Vêkanandre Sthephany Smith Brasil de Paitê"));
-            add(new Questao(R.drawable.jesus, "Qual o refrão da música que essa missionária estava cantando?", R.id.rbResposta3, "Sem Jesus não viverão!", "Jesus é bom o tempo todo", "O sangue de Jesus tem poder!", "O sangue de Jesus é maravilhosooooo!"));
+            add(new Questao(R.drawable.questao1, "Considerando o pseudocódigo", R.id.rbResposta3, "A) Lê um número negativo e mostra na tela o seu fatorial.", "B) Lê o fatorial e mostra o número na tela.", "C) Lê um número não negativo e mostra na tela o seu fatorial.", "D) Nenhuma das opções acima."));
+            add(new Questao(R.drawable.questao2,"Analise os trechos de código acima, escritos em uma linguagem de programação hipotética, fazendo uso dos comandos “while-do” (enquanto-faça) e “do-while” (faça-enquanto) e supondo que a, b e c foram declaradas anteriormente." + "\n \nQuais são os valores de a, b e c, após o término de cada trecho, se as variáveis a, b e c foram inicializadas com 3, 0 e 3, respectivamente, antes de cada trecho?", R.id.rbResposta1, "A) 3, 0 e 3   e   2, 1 e 4.", "B) 2, 1 e 1   e   2, 1 e 2.", "C) 1, 2 e 3   e   3, 2 e 3.", "D) 2, 1 e 3   e   1, 2 e 3."));
+            add(new Questao(R.drawable.questao3,"Considere o seguinte algoritmo, onde n é um inteiro positivo lido do teclado:", R.id.rbResposta4, "A) Constante;", "B) Logarítmica em n;", "C) Linear em n;", "D) Quadrática em n;"));
+            add(new Questao(R.drawable.questao4, "Sendo a e b variáveis inteiras em um programa, a expressão lógica", R.id.rbResposta3, "A) (a <= b);", "B) (a >= b);", "C) (a < b);", "D) (a > b);"));
+            add(new Questao(R.drawable.questao5, "Considere o código acima:", R.id.rbResposta2, "A) Em um laço de repetição, o controle do número de vezes que o laço será repetido ocorre por meio de variáveis", "B) Em um laço de repetição, o controle do número de vezes que o laço será repetido ocorre por meio de operadores lógicos.", "C) Opção A e B.", "D) Nenhuma das alternativas."));
+            add(new Questao(R.drawable.logo, "Mamãe, no céu tem pão?", R.id.rbResposta3, "E sobreviveu", "E viveu", "E morreu", "E faleceu"));
+            add(new Questao(R.drawable.logo, "Hoje eu vim falar de um assunto polêmico..", R.id.rbResposta4, "Nádegas", "Olhos", "Mãos", "Mamilos"));
+            add(new Questao(R.drawable.logo, "Complete a frase segundo Gilderlan. E depois.. tem:", R.id.rbResposta1, "Xenhenhenhenhem", "Nheco Nheco", "Nananananana", "Um furdunço"));
+            add(new Questao(R.drawable.logo, "Qual é o nome artístico dessa maravilhosa criatura?", R.id.rbResposta2, "Lohane Vêkanandre Gisele Gavião da Portela de bala Halls", "Lohane Vêkanandre Sthephany Smith Bueno de HA HA HA de Raio Laser bala de Icekiss", "Lohane Vêkanandre Bueno de HA HA HA de Raio Laser de bala Mentos", "Lohane Vêkanandre Sthephany Smith Brasil de Paitê"));
+            add(new Questao(R.drawable.logo, "Qual o refrão da música que essa missionária estava cantando?", R.id.rbResposta3, "Sem Jesus não viverão!", "Jesus é bom o tempo todo", "O sangue de Jesus tem poder!", "O sangue de Jesus é maravilhosooooo!"));
 
 
         }
@@ -72,13 +72,13 @@ public class QuizActivity extends AppCompatActivity {
         else intent.putExtra("acertou", false);
         intent.putExtra("pontos", pontos);
         startActivity(intent);
-        
+
         rgRespostas.clearCheck();
     }
 
     private void carregarQuestao(){
 
-        embaralhar(questoes);
+        // embaralhar(questoes);
 
         if(questoes.size() > 0) {
             Questao q = questoes.remove(0);
